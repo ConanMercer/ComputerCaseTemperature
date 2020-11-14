824291f445cef0d11d27d8a1c884876b2c97b74c
Conan <conan.mercer1@gmail.com>
2020-11-14 16:54:00 +0100

    -add, IR Temp sensor. - add, associated library. - add, simple routine to test temp output

--- src/Oled.ino
+++ src/Oled.ino
@@ -1,9 +1,16 @@
+// --- Libraries
 #include <Arduino.h>
 #include <U8g2lib.h>
 #include <Wire.h>
+#include <Adafruit_MLX90614.h> // Library to communicate with IR sensors
 
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE); //sets to correct type of oled screen
+// --- Variables and settings
 
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE);
+Adafruit_MLX90614 IR_1 = Adafruit_MLX90614(0x5A);
+double TEMP;
+
+// --- Voids
 void TCA9548A(uint8_t bus)
 {
   Wire.beginTransmission(0x71); // TCA9548A address is 0x70
@@ -36,21 +43,27 @@ void description(void)
 void setup()
 {
   Wire.begin();
-  u8g2.begin();
-  TCA9548A(1);   // tell the TCA9548A we want to use I2C bus number 1 (to talk to the OLED)
-  description(); // clear the internal memory
-  TCA9548A(2);
-  hello();
-  TCA9548A(3);
-  hello();
-  TCA9548A(5);
-  hello();
-  TCA9548A(6);
-  hello();
-  TCA9548A(7);
-  hello();
+  IR_1.begin(); // Initialise IR sensor
+  Serial.begin(9600);
+  // u8g2.begin();
+  // TCA9548A(1);   // tell the TCA9548A we want to use I2C bus number 1 (to talk to the OLED)
+  // description(); // clear the internal memory
+  // TCA9548A(2);
+  // hello();
+  // TCA9548A(3);
+  // hello();
+  // TCA9548A(5);
+  // hello();
+  // TCA9548A(6);
+  // hello();
+  // TCA9548A(7);
+  // hello();
 }
 
 void loop()
 {
+  TEMP = IR_1.readObjectTempC();
+  delay(1000);
+  Serial.print(TEMP);
+  Serial.println("");
 }
