020698d9c7ee349f25d30dfedb5047f084e93ff4
Conan <conan.mercer1@gmail.com>
2020-11-15 16:54:17 +0100

    -add, degree Celsius symbol. - update, font and font size. - update, font now set once in setup.

--- src/Oled.ino
+++ src/Oled.ino
@@ -20,11 +20,12 @@ void TCA9548A(uint8_t bus)
 
 void temp(void)
 {
-  TEMP = IR_1.readObjectTempC();                     // store temp in a double
-  u8g2.clearBuffer();                                // clear the internal memory
-  u8g2.setFont(u8g2_font_ncenB08_tr);                // choose a suitable font
-  u8g2.drawStr(25, 10, dtostrf(TEMP, 7, 3, outstr)); // convert double to string
-  u8g2.sendBuffer();                                 // transfer internal memory to the display
+  TEMP = IR_1.readObjectTempC();                    // store temp in a double
+  u8g2.clearBuffer();                               // clear the internal memory
+  u8g2.drawStr(0, 50, dtostrf(TEMP, 5, 1, outstr)); // convert double to string
+  u8g2.drawStr(80, 50, "\xb0");                     // hex code for degrees celsius
+  u8g2.drawStr(84, 50, " C");
+  u8g2.sendBuffer(); // transfer internal memory to the display
 }
 
 void description(void)
@@ -32,7 +33,6 @@ void description(void)
   Wire.begin();
   u8g2.begin();
   u8g2.clearBuffer();
-  u8g2.setFont(u8g2_font_ncenB08_tr);
   u8g2.drawStr(10, 10, "Infrared Temperature");
   u8g2.drawStr(10, 30, "Sensing");
   u8g2.drawStr(10, 50, "Platform");
@@ -41,9 +41,10 @@ void description(void)
 
 void setup()
 {
-  u8g2.setBusClock(100000); // set I2C to 100KHz so it is compatable with MLX90614
-  u8g2.begin();             // Initialise OLED screens
-  IR_1.begin();             // Initialise IR sensor
+  u8g2.setBusClock(100000);           // set I2C to 100KHz so it is compatable with MLX90614
+  u8g2.begin();                       // Initialise OLED screens
+  IR_1.begin();                       // Initialise IR sensor
+  u8g2.setFont(u8g2_font_helvR24_tf); // choose a suitable font
 }
 
 void loop()
