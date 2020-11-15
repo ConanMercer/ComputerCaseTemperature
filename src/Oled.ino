bc103926f2b71988948f2324de7b962564eb596a
Conan <conan.mercer1@gmail.com>
2020-11-15 15:31:07 +0100

    -add, set the u8g2 bus clock to 100KHz so it is now compatible with the IR sensor. -add, static char to hold converted double inside. -remove, now obsolete voids.

--- src/Oled.ino
+++ src/Oled.ino
@@ -5,27 +5,26 @@
 #include <Adafruit_MLX90614.h> // Library to communicate with IR sensors
 
 // --- Variables and settings
-
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE);
-Adafruit_MLX90614 IR_1 = Adafruit_MLX90614(0x5A);
+Adafruit_MLX90614 IR_1 = Adafruit_MLX90614(0x5A); // MLX90614 address is 0x5A
 double TEMP;
+static char outstr[15];
 
 // --- Voids
 void TCA9548A(uint8_t bus)
 {
-  Wire.beginTransmission(0x71); // TCA9548A address is 0x70
+  Wire.beginTransmission(0x71); // TCA9548A address is 0x71
   Wire.write(1 << bus);         // send byte to select bus
   Wire.endTransmission();
 }
 
-void hello(void)
+void temp(void)
 {
-  Wire.begin();
-  u8g2.begin();
-  u8g2.clearBuffer();                   // clear the internal memory
-  u8g2.setFont(u8g2_font_ncenB08_tr);   // choose a suitable font
-  u8g2.drawStr(25, 10, "Hello World!"); // write something to the internal memory
-  u8g2.sendBuffer();                    // transfer internal memory to the display
+  TEMP = IR_1.readObjectTempC();                     // store temp in a double
+  u8g2.clearBuffer();                                // clear the internal memory
+  u8g2.setFont(u8g2_font_ncenB08_tr);                // choose a suitable font
+  u8g2.drawStr(25, 10, dtostrf(TEMP, 7, 3, outstr)); // convert double to string
+  u8g2.sendBuffer();                                 // transfer internal memory to the display
 }
 
 void description(void)
@@ -42,28 +41,14 @@ void description(void)
 
 void setup()
 {
-  Wire.begin();
-  IR_1.begin(); // Initialise IR sensor
-  Serial.begin(9600);
-  // u8g2.begin();
-  // TCA9548A(1);   // tell the TCA9548A we want to use I2C bus number 1 (to talk to the OLED)
-  // description(); // clear the internal memory
-  // TCA9548A(2);
-  // hello();
-  // TCA9548A(3);
-  // hello();
-  // TCA9548A(5);
-  // hello();
-  // TCA9548A(6);
-  // hello();
-  // TCA9548A(7);
-  // hello();
+  u8g2.setBusClock(100000); // set I2C to 100KHz so it is compatable with MLX90614
+  u8g2.begin();             // Initialise OLED screens
+  IR_1.begin();             // Initialise IR sensor
 }
 
 void loop()
 {
-  TEMP = IR_1.readObjectTempC();
+  TCA9548A(1);
+  temp();
   delay(1000);
-  Serial.print(TEMP);
-  Serial.println("");
 }
